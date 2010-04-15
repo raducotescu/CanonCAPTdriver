ce5caf051659f52fa753c597a94923d1dfb30204
Radu Cotescu <radu@cotescu.com>
2010-04-15 18:16:37 +0300

    fixed a merge error

--- canonLBP_install.sh
+++ canonLBP_install.sh
@@ -23,11 +23,7 @@ LBP3108 LBP3150 LBP3200 LBP3210 LBP3250 LBP3300 LBP3310 LBP3500 LBP5000 LBP5050
 LBP5100 LBP5300 LBP6300dn LBP7200C LBP9100Cdn"
 
 usage_message="This script will help you install Canon CAPT Printer Driver \
-<<<<<<< HEAD:canonLBP_install.sh
 2.00 for Debian-based Linux systems using the 64-bit OS architecture.\n"
-=======
-1.90 for Debian-based Linux systems using the 32bit or 64bit OS architecture.\n"
->>>>>>> b85fe3774119a69b48055560ab0d5fc657dfced2:canonLBP_install.sh
 
 options="PRINTER_MODEL can be any of the following:\n$models"
 
